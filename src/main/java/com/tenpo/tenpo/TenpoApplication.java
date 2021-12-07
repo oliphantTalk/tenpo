8582efa1ab52c1a32cf3decfd26f745125ffa170
nanobarrena@gmail.com <nanobarrena@gmail.com>
2021-12-06 23:34:17 -0300

    First commit

---
+++ src/main/java/com/tenpo/tenpo/TenpoApplication.java
@@ -0,0 +1,13 @@
+package com.tenpo.tenpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenpoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tenpo/tenpo/TenpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenpo.tenpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenpoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
